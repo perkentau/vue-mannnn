f79d21ac1395ad93e175e5ff06062a0cd53f83c4
Permana-nurly <iniceo3521@gmail.com>
2026-02-10 23:25:15 -0500

    commit

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,9 +1,23 @@
 <template>
   <div>
     <center>
-      <h2>Halaman</h2>
+      <div class="card"></div>
+      <h2 class="btn btn-outline-light">Halaman</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minus maiores ut fugit? Tenetur quia repellat molestias incidunt, provident vero?</p>
-      <div class="btn-grad">home</div>
     </center>
     </div>
 </template>
+
+<style>
+  body {
+    background-image: linear-gradient(to right, #16222A 0%, #3A6073  51%, #16222A  100%)
+  }
+  .card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    height: 20rem;
+    width: 75rem;
+    margin: auto;
+    text-align: center;
+    font-family: arial;
+  }
+</style>
